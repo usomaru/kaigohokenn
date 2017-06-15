0199a0d79e15b63cc052134bc7e59562264ebecf
usomaru <s.ichikawa@uchida-it.co.jp>
2017-06-15 17:02:41 +0900

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MaintopComponent} from './maintop.component';
+import { ServiceComponent} from './service.component';
+import { MemoComponent} from './memo.component';
+
+import {Ng2BootstrapModule} from 'ng2-bootstrap/ng2-bootstrap';
+import { ModalModule} from 'ng2-bootstrap/modal';
+
+export const ROUTES = [
+ {path : 'Service' , component: ServiceComponent},
+ {path : 'Maintop', component:MaintopComponent},
+ {path: 'Memo', component:MemoComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServiceComponent,
+    MaintopComponent,
+    MemoComponent
+  ],
+  imports: [
+    Ng2BootstrapModule,
+    ModalModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(
+      ROUTES
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
